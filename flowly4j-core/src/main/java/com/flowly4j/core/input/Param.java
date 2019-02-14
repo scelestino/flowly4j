59c09de169ae45202c8f1fb5ab28ada703568265
Sebastian <scelestino@despegar.com>
2019-02-14 17:30:40 -0300

    change Param to Value

--- flowly4j-core/src/main/java/com/flowly4j/core/input/Param.java
+++ flowly4j-core/src/main/java/com/flowly4j/core/input/Param.java
@@ -2,17 +2,16 @@ package com.flowly4j.core.input;
 
 import lombok.*;
 
+
 /**
  * Represent a pair key -> value
  */
-@Getter
-@ToString
-@EqualsAndHashCode
+@Value
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Param {
 
-    private Key key;
-    private Object value;
+    Key key;
+    Object value;
 
     public static <T> Param of(Key<T> key, T obj) {
         return new Param(key, obj);
--- flowly4j-example/src/main/java/com/flowly4j/example/App.java
+++ flowly4j-example/src/main/java/com/flowly4j/example/App.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.flowly4j.core.output.ExecutionResult;
 import com.flowly4j.core.input.Param;
 import com.flowly4j.core.Workflow;
+import lombok.val;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,43 +22,42 @@ import static com.flowly4j.example.CustomKeys.KEY3;
  */
 public class App {
 
-
-    private static <T> TypeReference<T> bla() {
-            return new TypeReference<T>() {};
-    }
-
     public static void main( String[] args ) throws InterruptedException {
 
-        ExecutorService tpe = Executors.newFixedThreadPool(5);
+
+//        ExecutorService tpe = Executors.newFixedThreadPool(5);
 
         Workflow workflow = new WorkflowA();
 
         String sessionId = workflow.init(Param.of(KEY1, "asd"), Param.of(KEY2, 123));
 
-        tpe.execute(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            ExecutionResult result = workflow.execute(sessionId, Param.of(KEY3, true));
-            System.out.println(result);
-        });
+        ExecutionResult result = workflow.execute(sessionId, Param.of(KEY3, true));
+        System.out.println(result);
 
-        tpe.execute(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            ExecutionResult result = workflow.execute(sessionId);
-            System.out.println(result);
-        });
+//        tpe.execute(() -> {
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            ExecutionResult result = workflow.execute(sessionId, Param.of(KEY3, true));
+//            System.out.println(result);
+//        });
 
-
-        tpe.awaitTermination(1000, TimeUnit.MILLISECONDS);
-
-        tpe.shutdown();
+//        tpe.execute(() -> {
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            ExecutionResult result = workflow.execute(sessionId);
+//            System.out.println(result);
+//        });
+//
+//
+//        tpe.awaitTermination(1000, TimeUnit.MILLISECONDS);
+//
+//        tpe.shutdown();
 
         //ExecutionResult result = workflow.execute(sessionId);
 
