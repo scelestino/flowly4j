1974727654a22b27635a543a6574b84100cf6262
gianluca-capelo <gianluca.capelo@despegar.com>
2023-08-01 10:43:11 -0300

    remove main endpoints

--- flowly4j-examplemariadb/src/main/java/com/flowly4j/examplemariadb/App.java
+++ flowly4j-examplemariadb/src/main/java/com/flowly4j/examplemariadb/App.java
@@ -38,116 +38,6 @@ import static com.flowly4j.examplemariadb.CustomKeys.*;
  */
 public class App {
 
-    public static void main1(String[] args) {
-
-        val objectMapperContext = new ObjectMapper();
-        objectMapperContext.registerModule(new JavaTimeModule());
-        objectMapperContext.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
-        objectMapperContext.registerModule(new VavrModule(new VavrModule.Settings().deserializeNullAsEmptyCollection(true)));
-        objectMapperContext.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapperContext.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        val objectMapperRepository = new ObjectMapper();
-        objectMapperRepository.registerModule(new CustomDateModule());
-        objectMapperRepository.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
-        objectMapperRepository.registerModule(new VavrModule(new VavrModule.Settings().deserializeNullAsEmptyCollection(true)));
-        objectMapperRepository.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapperRepository.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("com.flowly4j.examplemariadb");
-
-        val repository = new MariaDBRepository(entityManagerFactory, objectMapperRepository);
-
-        Execution execution = new Execution("taskId", Instant.now(), Option.of(null));
-        Attempts attempts = new Attempts(1, Instant.now(), Option.of(Instant.now()));
-
-        /*repository.insert(new Session(
-                "sessionId", HashMap.of("variable_uno",
-                Product.of("transactionId", "type", "id"),
-                "variable_dos",
-                Product.of("transactionId2", "type2", "id2")), Option.of(execution), Option.of(attempts),
-                Instant.now(),
-                Status.FINISHED, 1L));*/
-
-        repository.update(new Session(
-                "sessionId", HashMap.of("variable_uno",
-                Product.of("transactionId", "type", "id"),
-                "variable_dos",
-                Product.of("transactionId2", "type2", "id2")), Option.of(execution), Option.of(attempts),
-                Instant.now(),
-                Status.FINISHED, 3L));
-
-        Option<Session> s = repository.get("sessionId");
-        s.forEach(System.out::println);
-
-        val serializer = new Serializer(objectMapperContext);
-        s.forEach(session -> {
-            session.getVariables().forEach((k, v) -> {
-                Product p = serializer.deepCopy(v, new TypeReference<Product>() {
-                });
-                System.out.println(p.getTransactionId());
-                System.out.println(p.getId());
-                System.out.println(p.getType());
-            });
-        });
-
-    }
-
-    public static void mainTestRetry(String[] args) {
-
-        val objectMapperContext = new ObjectMapper();
-        objectMapperContext.registerModule(new JavaTimeModule());
-        objectMapperContext.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
-        objectMapperContext.registerModule(new VavrModule(new VavrModule.Settings().deserializeNullAsEmptyCollection(true)));
-        objectMapperContext.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapperContext.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        val objectMapperRepository = new ObjectMapper();
-        objectMapperRepository.registerModule(new CustomDateModule());
-        objectMapperRepository.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
-        objectMapperRepository.registerModule(new VavrModule(new VavrModule.Settings().deserializeNullAsEmptyCollection(true)));
-        objectMapperRepository.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapperRepository.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("com.flowly4j.examplemariadb");
-
-        val repository = new MariaDBRepository(entityManagerFactory, objectMapperRepository);
-
-        Execution execution = new Execution("taskId", Instant.now(), Option.of(null));
-        Attempts attempts = new Attempts(1, Instant.now(), Option.of(Instant.now()));
-
-        repository.insert(new Session(
-                "1", HashMap.of("variable_uno",
-                Product.of("transactionId", "type", "id"),
-                "variable_dos",
-                Product.of("transactionId2", "type2", "id2")), Option.of(execution), Option.of(attempts),
-                Instant.now(),
-                Status.TO_RETRY, 1L));
-
-        repository.insert(new Session(
-                "15", HashMap.of("variable_uno",
-                Product.of("transactionId", "type", "id"),
-                "variable_dos",
-                Product.of("transactionId2", "type2", "id2")), Option.of(execution), Option.of(attempts),
-                Instant.now(),
-                Status.TO_RETRY, 1L));
-
-        repository.insert(new Session(
-                "2", HashMap.of("variable_uno",
-                Product.of("transactionId", "type", "id"),
-                "variable_dos",
-                Product.of("transactionId2", "type2", "id2")), Option.of(execution), Option.of(attempts),
-                Instant.now(),
-                Status.FINISHED, 1L));
-
-
-        Iterator<String> s = repository.getToRetry();
-        s.forEach(System.out::println);
-
-    }
-
     public static void main(String[] args) {
         //TODO SOLN: si usamos el mismo object mapper para repo y serializer funciona, Dory usa dos distintos
 
@@ -165,40 +55,10 @@ public class App {
         objectMapperRepository.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapperRepository.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);*/
 
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("com.flowly4j.examplemariadb");
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("com.flowly4j.examplemariadb");
 
         val repository = new MariaDBRepository(entityManagerFactory, objectMapperContext);
-
-        val factory = new ExecutionContext.ExecutionContextFactory(new Serializer(objectMapperContext));
-
-        System.out.println(repository.getToRetry().toList());
-
-        Workflow workflow = new WorkflowA(repository, factory);
-
-        String sessionId = workflow.init(Param.of(KEY1, "asd"), Param.of(KEY2, 123), Param.of(KEY6, Instant.EPOCH));
-
-        val result = workflow.execute(sessionId);
-
-        System.out.println(result);
-
-    }
-
-    public static void mainTestSessionFactory(String[] args) {
-        val objectMapperContext = new ObjectMapper();
-        objectMapperContext.registerModule(new JavaTimeModule());
-        objectMapperContext.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
-        objectMapperContext.registerModule(new VavrModule(new VavrModule.Settings().deserializeNullAsEmptyCollection(true)));
-        objectMapperContext.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapperContext.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("com.flowly4j.examplemariadb");
-
-        org.hibernate.Session session = entityManagerFactory.createEntityManager().unwrap(org.hibernate.Session.class);
-        SessionFactory sessionFactory = session.getSessionFactory();
-
-        val repository = new MariaDBRepository(sessionFactory, objectMapperContext);
 
         val factory = new ExecutionContext.ExecutionContextFactory(new Serializer(objectMapperContext));
 
